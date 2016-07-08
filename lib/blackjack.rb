fba32f82c64a6e432f1e6519eacb64452e8824f6
thejasonfile <jason.arnold.00@gmail.com>
2016-07-07 22:13:41 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,6 +64,8 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  card_total = hit?(card_total)
-  
+  until card_total > 21
+    card_total = hit?(card_total)
+  end
+  end_game(card_total)
 end
